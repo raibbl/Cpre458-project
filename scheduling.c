30a4ecff567bcc858994714e8b1fcfc4af395fb2
raibbl <raibbl1997@gmail.com>
2019-12-03 02:26:27 -0600

    kinda works rms not complepltey

--- scheduling.c
+++ scheduling.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <time.h>
 int numoftasks = 0;
 struct task
 {
@@ -10,6 +11,9 @@ struct task
     int runtime;  /* Time task requires to complete job */
     int priority; /* Priority of the task */
     int arrivaltime;
+    int nextarrivaltime;
+    int instancesP;    /*according to LCM how many are possible*/
+    int instancesleft; /*how many instances left*/
     /* Values algorithm may use to track tasks */
     int starttime;
     int endtime;
@@ -20,10 +24,8 @@ struct task
 
 /* Forward declarations of Scheduling algorithms */
 void first_come_first_served(struct task *proc);
-void shortest_remaining_time(struct task *proc);
+void rms(int numoftasks, struct task *proc);
 void checkfeasibility(int numoftasks, struct task *proc);
-
-
 
 int main()
 {
@@ -55,8 +57,8 @@ int main()
     //check schedubality
     // memcpy(proc_copy, proc, numoftasks * sizeof(struct task));
 
-    checkfeasibility(numoftasks, proc);
-   
+    // checkfeasibility(numoftasks, proc);
+    rms(numoftasks, proc);
 
     return 0;
 }
@@ -110,8 +112,8 @@ int findlowestp(int numoftasks, struct task *proc)
         double pi = proc[i].period;
 
         if (pi > max)
-        {   
-            max=pi;
+        {
+            max = pi;
             indexofmax = i;
         }
     }
@@ -146,39 +148,38 @@ int checkrms(int numoftasks, struct task *proc)
         int i = 0;
         int old_wc;
 
-       
-
         do
         {
-             old_wc=wc;
-            wc=0;
+            old_wc = wc;
+            wc = 0;
             if (i == 0)
             {
                 wc = cioflowestp; //just ci of lowest task priorty
                 i++;
             }
-            else if(i==1){
+            else if (i == 1)
+            {
                 int j;
 
                 for (j = 0; j < numoftasks; j++)
                 {
                     if (j != taskwithlowestpindex)
                     {
-                        
-                        
+
                         double othertaskpi = proc[j].period;
                         //printf("  %lf",othertaskpi);
-                        double ratio=(cioflowestp/othertaskpi);
-                       // printf("  %lf",ratio);
-                        
-                        wc = wc+ceil(ratio)*proc[j].runtime;
+                        double ratio = (cioflowestp / othertaskpi);
+                        // printf("  %lf",ratio);
+
+                        wc = wc + ceil(ratio) * proc[j].runtime;
                     }
                 }
                 i++;
 
                 wc = wc + cioflowestp;
-                 printf("  %d",wc);
-                if(wc>pioflowestp){ 
+                printf("  %d", wc);
+                if (wc > pioflowestp)
+                {
                     //not schduable
                     return 0;
                 }
@@ -192,27 +193,27 @@ int checkrms(int numoftasks, struct task *proc)
                 {
                     if (j != taskwithlowestpindex)
                     {
-                        
-                       
+
                         double othertaskpi = proc[j].period;
                         //printf("  %lf",othertaskpi);
-                        double ratio=(old_wc/othertaskpi);
+                        double ratio = (old_wc / othertaskpi);
                         //printf("  %lf",ratio);
-                        
-                        wc = wc+ceil(ratio)*proc[j].runtime;
+
+                        wc = wc + ceil(ratio) * proc[j].runtime;
                     }
                 }
                 i++;
 
                 wc = wc + cioflowestp;
-                printf("  %d",wc);
-                if(wc>pioflowestp){ 
+                printf("  %d", wc);
+                if (wc > pioflowestp)
+                {
                     //not schduable
                     return 0;
                 }
             }
 
-        } while (wc !=old_wc);
+        } while (wc != old_wc);
 
         return 1; //exact analysis confirms test
     }
@@ -242,16 +243,50 @@ void checkfeasibility(int numoftasks, struct task *proc)
     }
 }
 
-void first_come_first_served(struct task *proc)
+/*helper function to find gcd for lcm*/
+int gcd(int a, int b)
 {
-    //sort task first by arrival time
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+/*function to find LCM of tasks*/
+
+int lcmtasks(int numoftasks, struct task *proc)
+{
+    int res = 1, i;
+    for (i = 0; i < numoftasks; i++)
+    {
+        res = res * proc[i].period / gcd(res, proc[i].period);
+    }
+    return res;
+}
+
+/*are all tasks done? if 1 then yes, 0 if no */
+int alltasksdone(int numoftasks, struct task *proc)
+{
+    int i = 0;
+    for (i = 0; i < numoftasks; i++)
+    {
+        if (!(proc[i].instancesleft == 0))
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void rms(int numoftasks, struct task *proc)
+{
+    //sort task first by period
     int i = 0;
     for (i = 0; i < numoftasks; i++)
     {
         int j = 0;
         for (j = i; j < numoftasks; j++)
         {
-            if (proc[i].arrivaltime >= proc[j].arrivaltime)
+            if (proc[i].period >= proc[j].period)
             {
 
                 struct task temp;
@@ -261,271 +296,162 @@ void first_come_first_served(struct task *proc)
             }
         }
     }
-    //now print them
-    int time = proc[0].arrivaltime;
-    int avgsum = 0;
+
+    //find LCM
+
+    double lcm = lcmtasks(numoftasks, proc);
+
+    printf(" LCM = %lf\n", lcm);
+
+    //how many insances of each task? && iti;ize each task
     for (i = 0; i < numoftasks; i++)
     {
-
-        proc[i].starttime = time;
-        time = time + proc[i].runtime;
-        proc[i].endtime = time;
-        avgsum += (proc[i].endtime - proc[i].arrivaltime);
-        printf("Process %d started at time %d\n", proc[i].orignalindex, proc[i].starttime);
-        printf("Process %d finished at time %d\n", proc[i].orignalindex, proc[i].endtime);
-    }
-    printf("Average time from arrival to finish is %d seconds", avgsum / numoftasks);
-}
-
-void shortest_remaining_time(struct task *proc)
-{
-    int i = 0;
-    int avgsum = 0;
-    for (i = 0; i < numoftasks; i++)
-    {
-
-        int j = 0;
-        for (j = i; j < numoftasks; j++)
-        {
-            if (proc[i].arrivaltime >= proc[j].arrivaltime)
-            {
-                struct task temp;
-                temp = proc[i];
-                proc[i] = proc[j];
-                proc[j] = temp;
-            }
-        }
+        double taskperiod = proc[i].period;
+        proc[i].instancesP = floor(lcm / taskperiod);
+        printf("instance::: %d", proc[i].instancesP);
+        proc[i].instancesleft = proc[i].instancesP;
+        proc[i].remainingtime = proc[i].runtime;
+        proc[i].arrivaltime = proc[i].period;
     }
 
-    int time = 0;
-    int saveindex;
-    int all = numoftasks;
-    int shortest = 0;
+    //RMS schduler:
+    int time = 0; //track time through scheduling
+    i = 0;
 
-    while (all != 0)
+    while (time != lcm)
     {
-        saveindex = -1;
-        shortest = 100000000;
-
-        for (i = 0; i < numoftasks; i++)
+        if (alltasksdone(numoftasks, proc) == 1)
         {
-            if (proc[i].arrivaltime <= time && !proc[i].flag &&
-                proc[i].remainingtime < shortest)
-            {
-                saveindex = i;
-                shortest = proc[i].remainingtime;
-            }
+            break;
+        }
+        printf("LCM:::%lf\n", lcm);
+        printf("TIME:::%d\n", time);
+        printf("IIIII:::%d\n", i);
+        //loop all sorted tasks
+
+        //check if we have an idle status
+        if (i == numoftasks)
+        {
+
+            i = 0;
+            printf("iIIIIMEEMEM %d\n", i);
+            //time++;
         }
 
-        if (saveindex > -1)
+        while (i < numoftasks)
         {
-            if (proc[saveindex].remainingtime == proc[saveindex].runtime)
+            int prempit = 0;
+            proc[i].nextarrivaltime = proc[i].arrivaltime + proc[i].period;
+            printf("nextarrivaltime %d\n", proc[i].nextarrivaltime);
+            int timetocheck = -1;
+            //check if we need to check antything before this priorty wise
+
+            //checking priorty
+            int x = 1;
+            while (i - x >= 0)
             {
-                proc[saveindex].starttime = time;
-                printf("Process %d started at time %d\n", proc[saveindex].orignalindex, proc[saveindex].starttime);
+                //if this is not the priorty task
+                timetocheck = proc[i - x].arrivaltime; //set the time to check for the last pririty task.
+                x++;
             }
 
-            proc[saveindex].remainingtime -= 1;
-            time += 1;
+            printf("%d ARRIVAL TIME\n", proc[i].arrivaltime);
+            printf("%d check TIME\n", timetocheck);
 
-            if (proc[saveindex].remainingtime == 0)
+            if (proc[i].arrivaltime == time)
             {
-                proc[saveindex].flag = 1;
-                proc[saveindex].endtime = time;
-                all--;
-                printf("Process %d finished at time %d\n", proc[saveindex].orignalindex, proc[saveindex].endtime);
-
-                avgsum += proc[saveindex].endtime - proc[saveindex].arrivaltime;
+                proc[i].flag = 00;
             }
-        }
-        else
-        {
-            time++;
-        }
-    }
-
-    printf("Average time from arrival to completion is %d seconds\n",
-           avgsum / numoftasks);
-}
-
-void round_robin(struct task *proc)
-{
-    int i;
-    int avgsum = 0;
-    int time = 0;
-    int q = 10;
-    int all = numoftasks;
-    int saveindx;
-    int current = 0;
-
-    for (i = 0; i < numoftasks; i++)
-    {
-
-        int j = 0;
-        for (j = i; j < numoftasks; j++)
-        {
-            if (proc[i].arrivaltime >= proc[j].arrivaltime)
+            if (proc[i].flag != 12)
             {
-                struct task temp;
-                temp = proc[i];
-                proc[i] = proc[j];
-                proc[j] = temp;
+                proc[i].starttime = time;
             }
-        }
-    }
 
-    while (all != 0)
-    {
-        saveindx = -1;
-
-        for (i = current + 1; i < numoftasks; i++)
-        {
-            if (proc[i].arrivaltime <= time && !proc[i].flag)
+            //printf("STARTTIME %d\n",proc[i].starttime);
+            //schedule while there is ci left and there no priorty task arriving
+            while (proc[i].remainingtime != 0 && timetocheck != time && proc[i].instancesleft > 0)
             {
-                saveindx = i;
-                break;
-            }
-        }
-
-        if (saveindx < 0)
-        {
-            current = 0;
-            for (i = current; i < numoftasks; i++)
-            {
-                if (proc[i].arrivaltime <= time && !proc[i].flag)
+                //printf("im here");
+                if (proc[i].flag != 12)
                 {
-                    saveindx = i;
-                    break;
+                    printf("START TIME RIGHT NOW IS %d\n", proc[i].starttime);
+
+                    time = time + 1;
+                    proc[i].remainingtime = proc[i].remainingtime - 1;
+                }
+                else
+                {
+
+                   
+                    
+                        time++;
+                        if ((proc[i].arrivaltime == time))
+                        {
+                            proc[i].flag = 00;
+                            proc[i].starttime = time;
+                        }
+                    }
+
+                    //if we reached a higher priorty
+                    if (time == timetocheck && (i - 1 >= 0))
+                    {
+                        //printf("im here");
+                        proc[i].endtime = time;
+                        //go back to more priorty task
+                        printf("Task %d,started at time %d and ended at time %d hmm\n ", i, proc[i].starttime, proc[i].endtime);
+
+                        //if ended this instance ended
+                        if (proc[i].remainingtime == 0)
+                        {
+
+                            proc[i].remainingtime = proc[i].runtime;
+
+                            proc[i].flag = 12; //to mark its done this instance
+                            if (proc[i].instancesleft == proc[i].instancesP)
+                            {
+                                proc[i].arrivaltime = proc[i].period;
+                            }
+                            else
+                            {
+                                proc[i].arrivaltime = proc[i].arrivaltime + proc[i].period;
+                            }
+                            proc[i].instancesleft--;
+                        }
+
+                        i--;
+                        prempit = 1;
+                        timetocheck = -1;
+
+                        break;
+                    }
+                }
+
+                if (prempit != 1)
+                {
+                    proc[i].endtime = time;
+                    //if ended this instance ended
+                    if (proc[i].remainingtime == 0)
+                    {
+                        proc[i].remainingtime = proc[i].runtime;
+
+                        proc[i].flag = 12; //to mark its done this instance
+                        if (proc[i].instancesleft == proc[i].instancesP)
+                        {
+                            proc[i].arrivaltime = proc[i].period;
+                        }
+                        else
+                        {
+                            proc[i].arrivaltime = proc[i].arrivaltime + proc[i].period;
+                        }
+                        proc[i].instancesleft--;
+                    }
+                    if (proc[i].starttime != proc[i].endtime)
+                    {
+                        printf("Task %d,started at time %d and ended at time %d \n", i, proc[i].starttime, proc[i].endtime);
+                    }
+                    i++;
                 }
             }
-        }
-
-        if (saveindx > -1)
-        {
-            if (proc[saveindx].remainingtime == proc[saveindx].runtime)
-            {
-
-                proc[saveindx].starttime = time;
-                printf("Process %d started at time %d\n", saveindx, proc[saveindx].starttime);
-            }
-            if (proc[saveindx].remainingtime <= q)
-            {
-
-                time += proc[saveindx].remainingtime;
-                proc[saveindx].remainingtime = 0;
-                proc[saveindx].flag = 1;
-                proc[saveindx].endtime = time;
-                all--;
-                printf("Process %d finished at time %d\n", saveindx, proc[saveindx].endtime);
-
-                avgsum += proc[saveindx].endtime - proc[saveindx].arrivaltime;
-            }
-            else
-            {
-
-                proc[saveindx].remainingtime -= q;
-                time += q;
-            }
-            current = saveindx;
-        }
-        else
-        {
-            time++;
-            current = 0;
+            // sleep(2);
         }
     }
-
-    printf("Average time from arrival to completion is %d seconds\n",
-           avgsum / numoftasks);
-}
-
-void round_robin_priority(struct task *proc)
-{
-    int i;
-    int avgsum = 0;
-    int time = 0;
-    int q = 10;
-    int all = numoftasks;
-    int saveindx;
-    int current = 0;
-    int highest_priority;
-
-    for (i = 0; i < numoftasks; i++)
-    {
-
-        int j = 0;
-        for (j = i; j < numoftasks; j++)
-        {
-            if (proc[i].arrivaltime >= proc[j].arrivaltime)
-            {
-                struct task temp;
-                temp = proc[i];
-                proc[i] = proc[j];
-                proc[j] = temp;
-            }
-        }
-    }
-
-    while (all != 0)
-    {
-        saveindx = -1;
-
-        highest_priority = 0;
-        for (i = 0; i < numoftasks; i++)
-        {
-            if (proc[i].arrivaltime <= time && !proc[i].flag &&
-                highest_priority < proc[i].priority)
-            {
-                highest_priority = proc[i].priority;
-                break;
-            }
-        }
-
-        for (i = 0; i < numoftasks; i++)
-        {
-            if (proc[i].arrivaltime <= time && !proc[i].flag &&
-                highest_priority == proc[i].priority)
-            {
-                saveindx = i;
-                break;
-            }
-        }
-
-        if (saveindx > -1)
-        {
-            if (proc[saveindx].remainingtime == proc[saveindx].runtime)
-            {
-
-                proc[saveindx].starttime = time;
-                printf("Process %d started at time %d\n", saveindx, proc[saveindx].starttime);
-            }
-            if (proc[saveindx].remainingtime <= q)
-            {
-
-                time += proc[saveindx].remainingtime;
-                proc[saveindx].remainingtime = 0;
-                proc[saveindx].flag = 1;
-                proc[saveindx].endtime = time;
-                all--;
-                printf("Process %d finished at time %d\n", saveindx, proc[saveindx].endtime);
-
-                avgsum += proc[saveindx].endtime - proc[saveindx].arrivaltime;
-            }
-            else
-            {
-
-                proc[saveindx].remainingtime -= q;
-                time += q;
-            }
-            current = saveindx;
-        }
-        else
-        {
-            time++;
-            current = 0;
-        }
-    }
-
-    printf("Average time from arrival to completion is %d seconds\n",
-           avgsum / numoftasks);
-}
